3d22d046a2552ad44d6d203bb87f83a89fb6031e
Gerard Nantel <gerard.nantel@nuance.com>
2022-03-31 14:13:54 +0000

    Use composite actions to avoid code duplication

--- .github/scripts/output-last-workflow-run-number.sh
+++ .github/scripts/output-last-workflow-run-number.sh
@@ -2,15 +2,16 @@
 
 WORKFLOW=$1
 REPOSITORY=$2
+BRANCH=$3
 
-if [[ -z $WORKFLOW || -z $REPOSITORY ]]; then
-  echo "Usage: $0 <workflow> <repository-with-owner>"
-  echo "Examples: $0 nlu-runtime-service nuance-internal/ct-platform-nlu-nle"
-  echo "          $0 nlu-runtime-service \${github.repository}"
+if [[ -z $WORKFLOW || -z $REPOSITORY || -z $BRANCH ]]; then
+  echo "Usage: $0 <workflow> <repository-with-owner> <branch>"
+  echo "Examples: $0 nlu-runtime-service nuance-internal/ct-platform-nlu-nle main"
+  echo "          $0 nlu-runtime-service \${github.repository} main"
   exit 1
 fi
 
-API_QUERY="repos/$REPOSITORY/actions/workflows/.github%2fworkflows%2f$WORKFLOW.yml/runs?per_page=1&branch=main"
+API_QUERY="repos/$REPOSITORY/actions/workflows/.github%2fworkflows%2f$WORKFLOW.yml/runs?per_page=1&branch=$BRANCH"
 
 RUN_NUMBER=$(gh api $API_QUERY --jq ".workflow_runs[].run_number")
 echo "::set-output name=last-run-number::$RUN_NUMBER"
